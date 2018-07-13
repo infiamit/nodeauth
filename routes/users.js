2dea969d6bb72f443d0dcab2bedc8d6407c0bbe4
infiamit <amitsingh80108@gmail.com>
2018-07-13 14:25:26 +0530

    third

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var User = require('../models/user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource2');
+  res.send('respond with a resource3');
 });
 
 router.get('/registration', function(req, res, next) {
